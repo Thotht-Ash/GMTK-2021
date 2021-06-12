f1d9f78d4b621809ac9e0480a9041c54e51dfadf
Steve <bjarnekam@web.de>
2021-06-12 17:18:08 +0200

    Killing features left right center. also the ball isnt a bitch anymore... i GUESS

--- Assets/Scenes/Playercontroller.cs
+++ Assets/Scenes/Playercontroller.cs
@@ -1,6 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class ObjectEvent : UnityEvent <object> { }
 
 public class Playercontroller : MonoBehaviour
 {
@@ -10,27 +14,30 @@ public class Playercontroller : MonoBehaviour
     [Range(0, 100f)] [SerializeField] private float snapback = 0.1f;
     [SerializeField] private Transform groundcheck;
     [SerializeField] private float balldist = 20f;
-    // optional? [SerializeField] private Transform balldstcheck;
     [SerializeField] private LayerMask groundtype;
     [SerializeField] private GameObject ball = null;
-    // mb similar thing for ball?
 
     private bool grounded;
     private bool carrying;
-    private bool[] locked = { false, false, false, false }; //up down left right
     private Vector2 velocity = Vector2.zero;
     private bool facingR = true;
     private Rigidbody2D body;
+    private float distV;
 
+
+    public ObjectEvent BallPullEvent;
     // Update is called once per frame
 
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
+        if (BallPullEvent == null)
+        {
+            BallPullEvent = new ObjectEvent();
+        }
     }
     void FixedUpdate()
     {
-        Vector3 distV;
         //bool wasGrounded = grounded;
         grounded = false;
 
@@ -43,52 +50,7 @@ public class Playercontroller : MonoBehaviour
             }
         }
 
-        distV = groundcheck.position - ball.transform.position;
-        Debug.Log(distV.magnitude);
-        if (distV.magnitude >= balldist)
-        {
-            if (distV.x >= 0)// Right
-            {
-                locked[3] = true;
-            }
-            else
-            {
-                locked[3] = false;
-            }
-            if (distV.x <= 0) //no elif because it spares explicit handling of x=0
-            {   // left
-                locked[2] = true;
-            } else
-            {
-                locked[2] = false;
-            }
-
-            if (distV.y >= 0)
-            {
-                locked[0] = true;
-            } else
-            {
-                locked[0] = false;
-            }
-            if (distV.y <= 0) //no elif because it spares explicit handling of x=0
-            {
-                locked[1] = true;
-            } else
-            {
-                locked[1] = false;
-            }
-            // z *should* always be zero
-            if (distV.z != 0)
-            {
-                Debug.Log("WTF? distV.z should never be non 0");
-            }
-        } else
-        {
-            for (int i = 0; i < locked.Length; i++)
-            {
-                locked[i] = false;
-            }
-        }
+        distV = (groundcheck.position - ball.transform.position).magnitude;
         Vector2 v = body.velocity;
 
         body.velocity = v;
@@ -125,24 +87,17 @@ public class Playercontroller : MonoBehaviour
             }
         }
 
-        Vector2 targetV = new Vector2(move * 10f, body.velocity.y);
-        Vector2 smoothedV = Vector2.SmoothDamp(body.velocity, targetV, ref velocity, smoothing);
-        if (locked[0] && smoothedV.y > 0)
+        Vector2 targetV;
+        if (distV >= balldist)
         {
-            smoothedV.y = -snapback;
-        } else if (locked[1] && smoothedV.y < 0) // HERE BE HILARIOUS BUGS
-        {
-            smoothedV.y = snapback;
+            targetV = new Vector2(move * carryspeed * 10f, body.velocity.y);
         }
+        else
+        {
+            targetV = new Vector2(move * 10f, body.velocity.y);
+        }
+        body.velocity = Vector2.SmoothDamp(body.velocity, targetV, ref velocity, smoothing);
 
-        if (locked[2] && smoothedV.x < 0) //these should be less hilarious
-        {
-            smoothedV.x = snapback;
-        } else if (locked[3] && smoothedV.x > 0)
-        {
-            smoothedV.x = -snapback;
-        }
-        body.velocity = smoothedV;
 
         if (move > 0 && !facingR)
         {
@@ -154,10 +109,7 @@ public class Playercontroller : MonoBehaviour
         if (grounded && jump)
         {
             grounded = false;
-            if (!locked[0])
-            {
-                body.AddForce(new Vector2(0f, jumpforce));
-            }
+            body.AddForce(new Vector2(0f, (1-(distV-balldist))*jumpforce));
         }
     }
 
--- Assets/Scenes/ballpull.cs
+++ Assets/Scenes/ballpull.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static System.Math;
 
 
 public class ballpull : MonoBehaviour
 {
     [SerializeField] private GameObject man = null;
+    [SerializeField] private Rigidbody2D manbody = null;
     [SerializeField] private Transform groundcheck;
     [SerializeField] private float balldist = 20f;
     [Range(0, 0.3f)] [SerializeField] private float smoothing = 0.05f;
 
-
-
     private Vector2 smoothingV = Vector2.zero;
+    private Vector2 wasteV = Vector2.zero;
     private Rigidbody2D body;
 
 
@@ -21,14 +22,19 @@ public class ballpull : MonoBehaviour
         body = GetComponent<Rigidbody2D>();
     }
 
+
     // Update is called once per frame
     void FixedUpdate()
     {
-        body.velocity = Vector2.SmoothDamp(body.velocity, smoothingV, ref smoothingV, smoothing);
+        Vector2 distV = man.transform.position - body.transform.position;
+        if (distV.magnitude >= balldist && System.Math.Sign(manbody.velocity.x) == System.Math.Sign(distV.x)) // DO NOT QUESTION ME AGAIN MACHINE
+        {
+            body.velocity = manbody.velocity;
+        }
+        else
+        {
+            body.velocity = Vector2.SmoothDamp(body.velocity, smoothingV, ref wasteV, smoothing);
+        }
     }
 
-    public void MoveBall (Vector2 inv)
-    {
-        body.velocity = inv;
-    }
 }
